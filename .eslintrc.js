61e6bcb36255a17a12868396b2420fbf4f4a6661
Lucas Barroso <lucasbarroso23@gmail.com>
2021-05-08 00:57:04 -0300

    hashing user password

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
     'class-methods-use-this': 'off',
-    'no-params-reassign': 'off',
+    'no-param-reassign': 'off',
     camelcase: 'off',
     'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
     'linebreak-style': 'off',
